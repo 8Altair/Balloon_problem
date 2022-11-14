56105089e5235df54c13622077e971432a257b14
dino.ahmetbegovic <dino.ahmetbegovic@external.comtrade.com>
2022-11-14 22:48:58 +0100

    Bug fixes

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Solution
+{
+    public static int solution(String S)
+    {
+        HashMap<String, Integer> letterIterations = new HashMap<String, Integer>();
+        letterIterations.put("B", 0);
+        letterIterations.put("A", 0);
+        letterIterations.put("L", 0);
+        letterIterations.put("O", 0);
+        letterIterations.put("N", 0);
+
+        for(int i = 0; i < S.length(); i++)
+        {
+            if ((Character.toString(S.charAt(i)).equals("B")))
+            {
+                letterIterations.put("B", letterIterations.get("B") + 1);
+            }
+            if ((Character.toString(S.charAt(i)).equals("A")))
+            {
+                letterIterations.put("A", letterIterations.get("A") + 1);
+            }
+            if ((Character.toString(S.charAt(i)).equals("L")))
+            {
+                letterIterations.put("L", letterIterations.get("L") + 1);
+            }
+            if ((Character.toString(S.charAt(i)).equals("O")))
+            {
+                letterIterations.put("O", letterIterations.get("O") + 1);
+            }
+            if ((Character.toString(S.charAt(i)).equals("N")))
+            {
+                letterIterations.put("N", letterIterations.get("N") + 1);
+            }
+        }
+
+        return Math.min(letterIterations.get("B"), Math.min(letterIterations.get("A"),
+                Math.min(letterIterations.get("L") / 2, Math.min(letterIterations.get("O") / 2,
+                        letterIterations.get("N")))));
+    }
+}
