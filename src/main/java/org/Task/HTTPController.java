bda1b534b9fea6e4588746611b9e4e6efd337f85
8Altair <dinoa@comtrade.com>
2022-11-21 16:34:21 +0100

    Update

--- src/main/java/org/Task/HTTPController.java
+++ src/main/java/org/Task/HTTPController.java
@@ -1,19 +1,35 @@
 package org.Task;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
-import java.util.Enumeration;
-
-public class HTTPController
+public class HTTPController extends HttpServlet
 {
-    Enumeration<String> headerNames = request.getHeaderNames();
-    while(headerNames.hasMoreElements())
+    private int result;
+
+    public void init() throws ServletException
     {
-        String headerName = headerNames.nextElement();
-        System.out.println("Header Name - " + headerName + ", Value - " + request.getHeader(headerName));
+        // Do required initialization
+        result = Solution.solution("BALLOON", "BAOOLLNNOLOLGBAX");
     }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // Set response content type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + result + "</h1>");
+    }
+
+    public void destructor() { /* Do nothing */ }
 }
